b7ac125902ac1bd63ac4849a8d3d66c3986a0dac
Abhishek Roy <abhishek8400roy@gmail.com>
2025-12-11 17:46:14 +0530

    Inital projects page done

--- client/src/pages/MyProjects.tsx
+++ client/src/pages/MyProjects.tsx
@@ -1,14 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import type { Project } from '../types';
 import { Loader2Icon, PlusIcon } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
+import { dummyProjects } from '../assets/assets';
 
 const MyProjects = () => {
 
   const [loading,setLoading] = useState(true);
   const [projects,setProjects] = useState<Project[]>([])
+  const navigate = useNavigate()
 
   const fetchProjects = async ()=> {
+    setProjects(dummyProjects)
+// simulating loading thing
 
+setTimeout(()=>{
+setLoading(false)
+},1000)
   }
 
   useEffect(()=>{
@@ -20,17 +28,24 @@ fetchProjects()
       <div className='px-4 md:px-16 lg:px-24 xl:px-32'>
         {loading ? (
           <div className='flex items-center justify-center h-[80vh]'> 
-<Loader2Icon className='size-4 animate-spin text-indigo-200'/>
+<Loader2Icon className='size-7 animate-spin text-indigo-200'/>
           </div>
         ) : projects.length > 0 ? (
           <div className='py-10 min-h-[80vh]'> 
 <div className='flex items-center justify-between mb-12'>
-  <h1>My projects</h1>
-  <button><PlusIcon size={18}/> Create new</button>
+  <h1 className='text-2xl font-medium text-white'>My projects</h1>
+  <button onClick={()=> navigate('/')} className='flex items-center gap-3 text-white px-3 sm:px-6 
+  py-1 sm:py-2 rounded bg-linear-to-br from-indigo-500 to-indigo-600 hover:opacity-90 active:scale-95 transition-all
+  '><PlusIcon size={18}/> Create new</button>
 </div>
           </div>
         ) : (
-          <div></div>
+          <div className='flex flex-col items-center justify-center h-[80vh]'>
+<h1 className='text-3xl font-semibold text-gray-300'>You have no projects yet !</h1>
+<button onClick={()=> navigate('/')} className='text-white px-5 py-2 mt-5 rounded-md bg-indigo-500 hover:bg-indigo-600 active:scale-95 transition-all'>
+  Create new
+</button>
+          </div>
         )}
       </div>
     </>
