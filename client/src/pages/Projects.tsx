3842243893bd417d352070fd1c3fe74ec0b5da9c
Abhishek Roy <abhishek8400roy@gmail.com>
2025-12-13 13:17:17 +0530

    may be some responsivness error here . have to see it later .

--- client/src/pages/Projects.tsx
+++ client/src/pages/Projects.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import type { Project } from '../types'
-import { Loader2Icon, MessageSquareIcon, XIcon } from 'lucide-react'
+import { ArrowBigDownDashIcon, EyeOffIcon, FullscreenIcon, LaptopIcon, Loader2Icon, MessageSquareIcon, SaveIcon, SmartphoneIcon, TabletIcon, XIcon } from 'lucide-react'
 import { dummyConversations, dummyProjects } from '../assets/assets'
 
 
@@ -43,26 +43,48 @@ const Projects = () => {
     )
   }
   return project ? (
-    <div className='flex flex-col h-screen text-white w-full bg-gray-900'>
+    <div className='flex flex-col h-screen w-full bg-gray-900 text-white'>
       {/*buider navbar */}
       <div className='flex max-sm:flex-col sm:items-center gap-4 px-4 py-2 no-scrollbar'>
         {/* left */}
         <div className='flex items-center gap-2 sm:min-w-90 text-nowrap'>
-          <img src="/logom.png" alt="logo" className='h-6 cursor-pointer' onClick={()=> navigate('/')}/>
+          <img src="/logom.png" alt="logo" className='h-6 cursor-pointer' onClick={() => navigate('/')} />
           <div className='max-w-64 sm:max-w-xs'>
             <p className='text-sm text-medium capitalize truncate'>{project.name}</p>
             <p className='text-xs text-gray-400 -mt-0.5'>Previewing last saved version</p>
           </div>
           <div className='sm:hidden flex-1 flex justify-end'>
             {isMenuOpen ?
-             <MessageSquareIcon onClick={()=> setIsMenuOpen(false)} className='size-6 cursor-pointer'/> 
-            : <XIcon onClick={()=> setIsMenuOpen(true)} className='size-6 cursor-pointer'/>}
+              <MessageSquareIcon onClick={() => setIsMenuOpen(false)} className='size-6 cursor-pointer' />
+              : <XIcon onClick={() => setIsMenuOpen(true)} className='size-6 cursor-pointer' />}
           </div>
         </div>
         {/* middle */}
-        <div></div>
+        <div className='hidden sm:flex gap-2 bg-gray-950 p-1.5 rounded-md'>
+          <SmartphoneIcon onClick={() => setDevice('phone')} className={`size-6 p-1 rounded cursor-pointer ${device === 'phone' ? "bg-gray-700" : ""}`} />
+          <TabletIcon onClick={() => setDevice('tablet')} className={`size-6 p-1 rounded cursor-pointer ${device === 'tablet' ? "bg-gray-700" : ""}`} />
+          <LaptopIcon onClick={() => setDevice('desktop')} className={`size-6 p-1 rounded cursor-pointer ${device === 'desktop' ? "bg-gray-700" : ""}`} />
+        </div>
         {/* right */}
-        <div></div>
+        <div className='flex items-center justify-end gap-3 flex-1 text-xs sm:text-sm'>
+          <button disabled={isSaving} className='max-sm:hidden bg-gray-800 hover:bg-gray-700 text-white px-3.5 py-1 flex items-center gap-2 rounded sm:rounded-sm transition-colors border border-gray-700'>
+            {isSaving ? <Loader2Icon className='animate-spin' size={16} /> : <SaveIcon size={16} />} Save
+
+          </button>
+          <Link target='_blank' to={`/preview/${projectId}`} className='flex items-center gap-2 px-4 py-1 rounded sm:rounded-sm border border-gray-700 hover:border-gray-500 transition-colors'>
+            <FullscreenIcon size={16} /> Preview
+          </Link>
+          <button className='bg-linear-to-br from-blue-700 to-blue-600 hover:from-blue-600 hover:to-blue-500 text-white px-3.5 py-1 flex items-center gap-2 rounded sm:rounded-sm transition-colors'>
+            <ArrowBigDownDashIcon size={16} /> Download
+          </button>
+          <button className='bg-linear-to-br from-indigo-700 to-indigo-600 hover:from-indigo-600 hover:to-indigo-500 text-white px-3.5 py-1 flex items-center gap-2 rounded sm:rounded-sm transition-colors'>
+            {project.isPublished ?
+              <EyeOffIcon size={16} /> : <EyeOffIcon size={16} />
+            }
+            {project.isPublished ? "Unpublish" : "Publish"}
+          </button>
+
+        </div>
       </div>
     </div>
   )
