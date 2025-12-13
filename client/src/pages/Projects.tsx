aad438cdc705f42f7d63f068900102eebb104c27
Abhishek Roy <abhishek8400roy@gmail.com>
2025-12-13 09:29:50 +0530

    Left navbar done . onto the middle one .

--- client/src/pages/Projects.tsx
+++ client/src/pages/Projects.tsx
@@ -1,57 +1,77 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import type { Project } from '../types'
-import { Loader2Icon } from 'lucide-react'
+import { Loader2Icon, MessageSquareIcon, XIcon } from 'lucide-react'
 import { dummyConversations, dummyProjects } from '../assets/assets'
 
+
 const Projects = () => {
-  const {projectId} = useParams()
+  const { projectId } = useParams()
   const navigate = useNavigate()
 
-  const [project,setProject] = useState<Project | null>(null)
-  const [loading,setLoading] = useState(true)
+  const [project, setProject] = useState<Project | null>(null)
+  const [loading, setLoading] = useState(true)
 
-  const [isGenerating , setIsGenerating] = useState(true)
-  const [device , setDevice] = useState<'phone' | 'tablet' | 'desktop'>("desktop")
+  const [isGenerating, setIsGenerating] = useState(true)
+  const [device, setDevice] = useState<'phone' | 'tablet' | 'desktop'>("desktop")
 
-  const [isMenuOpen , setIsMenuOpen] = useState(false)
-  const [isSaving , setIsSaving] = useState(false)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [isSaving, setIsSaving] = useState(false)
 
-  const fetchProject = async ()=> {
-const project = dummyProjects.find(project => project.id === projectId)
-setTimeout(()=>{
-  if(project){
-    setProject({...project, conversation: dummyConversations});
-    setLoading(false)
-    setIsGenerating(project.current_code ? false : true)
+  const fetchProject = async () => {
+    const project = dummyProjects.find(project => project.id === projectId)
+    setTimeout(() => {
+      if (project) {
+        setProject({ ...project, conversation: dummyConversations });
+        setLoading(false)
+        setIsGenerating(project.current_code ? false : true)
+      }
+    }, 2000)
   }
-},2000)
-  }
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchProject()
-  },[])
-  
-  if(loading){
-    return(
+  }, [])
+
+  if (loading) {
+    return (
       <>
-      <div className='flex items-center justify-center h-screen'>
-        <Loader2Icon className='size-7 animate-spin text-violet-200'/>
-      </div>
+        <div className='flex items-center justify-center h-screen'>
+          <Loader2Icon className='size-7 animate-spin text-violet-200' />
+        </div>
       </>
     )
   }
   return project ? (
-    <div>
-      
+    <div className='flex flex-col h-screen text-white w-full bg-gray-900'>
+      {/*buider navbar */}
+      <div className='flex max-sm:flex-col sm:items-center gap-4 px-4 py-2 no-scrollbar'>
+        {/* left */}
+        <div className='flex items-center gap-2 sm:min-w-90 text-nowrap'>
+          <img src="/logom.png" alt="logo" className='h-6 cursor-pointer' onClick={()=> navigate('/')}/>
+          <div className='max-w-64 sm:max-w-xs'>
+            <p className='text-sm text-medium capitalize truncate'>{project.name}</p>
+            <p className='text-xs text-gray-400 -mt-0.5'>Previewing last saved version</p>
+          </div>
+          <div className='sm:hidden flex-1 flex justify-end'>
+            {isMenuOpen ?
+             <MessageSquareIcon onClick={()=> setIsMenuOpen(false)} className='size-6 cursor-pointer'/> 
+            : <XIcon onClick={()=> setIsMenuOpen(true)} className='size-6 cursor-pointer'/>}
+          </div>
+        </div>
+        {/* middle */}
+        <div></div>
+        {/* right */}
+        <div></div>
+      </div>
     </div>
   )
-  :
-  (
-    <div className='flex items-center justify-center h-screen'>
-<p className='text-2xl font-medium text-gray-200'>Unable to load project!</p>
-    </div>
-  )
+    :
+    (
+      <div className='flex items-center justify-center h-screen'>
+        <p className='text-2xl font-medium text-gray-200'>Unable to load project!</p>
+      </div>
+    )
 }
 
 export default Projects
