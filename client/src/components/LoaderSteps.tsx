224f8ba08d94f9f824914e18430035367e8c6ed1
Abhishek Roy <abhishek8400roy@gmail.com>
2025-12-26 19:35:21 +0530

    Downlaod button complete.

--- client/src/components/LoaderSteps.tsx
+++ client/src/components/LoaderSteps.tsx
@@ -1,4 +1,5 @@
 import { CircleIcon, ScalingIcon, SquareIcon, TriangleIcon } from "lucide-react"
+import { useEffect, useState } from "react"
 
 
 const steps = [
@@ -11,6 +12,17 @@ const steps = [
 const STEP_DURATION = 45000
 
 const LoaderSteps = () => {
+
+    const [current,setCurrent] = useState(0)
+
+    useEffect(()=>{
+        const interval = setInterval(()=>{
+            setCurrent((s)=>(s+1)% steps.length)
+        },STEP_DURATION);
+        return ()=> clearInterval(interval)
+    },[])
+    
+    const Icon = steps[current].icon
     return (
         <div className="w-full h-full flex flex-col items-center justify-center bg-gray-950 
     relative overflow-hidden text-white">
@@ -20,7 +32,7 @@ const LoaderSteps = () => {
             <div className="relative z-10 w-32 h-32 flex items-center justify-center">
                 <div className="absolute inset-0 rounded-full border border-indigo-400 animate-ping opacity-30" />
                 <div className="absolute inset-4 rounded-full border border-purple-400/20 " />
-                <div className="w-8 h-8 text-white opacity-80 animate-bounce " />
+                <Icon className="w-8 h-8 text-white opacity-80 animate-bounce " />
             </div>
             {/* step label - fade using transition */}
             <p key={current} className="mt-8 text-lg font-light text-white/90 
