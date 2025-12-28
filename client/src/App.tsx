01c9e5199d9b29997cfe20e3c4ca1b7fca490606
Abhishek Roy <abhishek8400roy@gmail.com>
2025-12-28 14:59:10 +0530

    Whole user authentication done . including deletion too . now onto the API integration .

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,6 +11,7 @@ import Home from './pages/Home'
 import Navbar from './components/Navbar'
 import { Toaster } from 'sonner'
 import AuthPage from './pages/auth/AuthPage'
+import Settings from './pages/Settings'
 
 const App = () => {
 
@@ -34,6 +35,7 @@ const App = () => {
         <Route path='community' element={<Community/>}/>
         <Route path='view/:projectId' element={<View/>}/>
          <Route path="/auth/:pathname" element={<AuthPage />} />
+        <Route path="/account/settings" element={<Settings />} />
       </Routes>
     </div>
   )
