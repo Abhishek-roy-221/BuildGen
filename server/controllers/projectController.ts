e4b2fad36a0215aa2e60b1f2aa728c4d4cb1079e
Abhishek Roy <abhishek8400roy@gmail.com>
2025-12-30 12:51:12 +0530

    Roll back version created .

--- server/controllers/projectController.ts
+++ server/controllers/projectController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import prisma from "../lib/prisma.js";
 import openai from '../configs/openai.js';
+import { log } from "node:console";
 
 //fn to make revision
 export const makeRevision = async (req: Request, res: Response) => {
@@ -164,3 +165,51 @@ Return ONLY the enhanced request, nothing else. Keep it concise (1-2 sentences).
         res.status(500).json({ message: error.message });
     }
 }
+
+//fn to Rollback 
+export const rollbackToVersion = async (req: Request, res: Response) => {
+  try {
+    const userId = req.userId;
+    if(!userId){
+        return res.status(401).json({message: 'Unauthorized'});
+    }
+
+    const {projectId ,versionId} = req.params;
+
+    const project = await prisma.websiteProject.findUnique({
+        where: {id: projectId,userId},
+        include: {versions: true}
+    })
+
+    if(!project){
+        return res.status(404).json({message: 'Project not found'});
+    }
+
+    const version = project.versions.find((version)=>version.id === versionId);
+
+    if(!version){
+        return res.status(404).json({message: 'Version not found'});
+    }
+
+    await prisma.websiteProject.update({
+        where: {id: projectId ,userId},
+        data: {
+            current_code: version.code,
+            current_version_index: version.id
+        }
+    })
+
+    await prisma.conversation.create({
+        data: {
+            role: 'assistant',
+            content: "I've rolled back your website to selected version . You can preview it",
+            projectId
+        }
+    })
+
+    res.json({message: 'Version rolled back'});
+  } catch (error : any) {
+    console.log(error.code || error.message);
+  }
+    
+} 
